67f125cdc4d234847c129e8dbf8a7ecdf24aadea
theVersu <101946187+theVersu@users.noreply.github.com>
2026-08-30 13:08:40 +0200

    Update Mobile & Desktop Chapter Navigation.js

--- Mobile & Desktop Chapter Navigation.js
+++ Mobile & Desktop Chapter Navigation.js
@@ -8,8 +8,8 @@
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
 // @run-at       document-end
-// @downloadURL  https://raw.githubusercontent.com/theVersu/hmm/refs/heads/main/Mobile & Desktop Chapter Navigation.js
-// @updateURL    https://raw.githubusercontent.com/theVersu/hmm/refs/heads/main/Mobile & Desktop Chapter Navigation.js
+// @downloadURL  https://raw.githubusercontent.com/theVersu/hmm/refs/heads/main/Mobile%20%26%20Desktop%20Chapter%20Navigation.js
+// @updateURL    https://raw.githubusercontent.com/theVersu/hmm/refs/heads/main/Mobile%20%26%20Desktop%20Chapter%20Navigation.js
 // ==/UserScript==
 
 (function() {
